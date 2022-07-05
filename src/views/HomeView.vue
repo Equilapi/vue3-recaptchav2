de9aa9e803bfe73ae88b880ceb40869df4cca331
Wentru <elvisquilapi@gmail.com>
2022-07-04 20:52:47 -0400

    Initial commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,25 @@
+<script>
+import { ref } from 'vue'
+import { VueRecaptcha } from 'vue-recaptcha';
+
+export default {
+  setup() {
+    const sitekey = ref(import.meta.env.VITE_RECAPTCHAV2_SITEKEY)
+    return {
+      sitekey
+    }
+  },
+  components: {
+    VueRecaptcha
+  }
+}
+
+
+</script>
+
+<template>
+  <main>
+    <!-- <TheWelcome /> -->
+    <vue-recaptcha :sitekey="sitekey"></vue-recaptcha>
+  </main>
+</template>
